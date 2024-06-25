e7f4ef035e65be7c51c019418c349bfc147f2259
yanghy <snxamdf@126.com>
2024-06-25 10:28:45 +0800

    v3: cef dev

--- cef/debug_most/v8context/v8context.go
+++ cef/debug_most/v8context/v8context.go
@@ -7,12 +7,12 @@ import (
 
 func Context(app cef.ICefApplication) {
 	var (
-		obj       cef.ICefv8Value
+		obj       cef.ICefV8Value
 		onHandler cef.IV8Handler
 	)
-	app.SetOnContextCreated(func(browser cef.ICefBrowser, frame cef.ICefFrame, context cef.ICefv8Context) {
+	app.SetOnContextCreated(func(browser cef.ICefBrowser, frame cef.ICefFrame, context cef.ICefV8Context) {
 		onHandler = cef.NewV8Handler()
-		onHandler.SetOnExecute(func(name string, object cef.ICefv8Value, arguments cef.ICefV8ValueArray) (retVal cef.ICefv8Value, exception string, result bool) {
+		onHandler.SetOnExecute(func(name string, object cef.ICefV8Value, arguments cef.ICefV8ValueArray) (retVal cef.ICefV8Value, exception string, result bool) {
 			fmt.Println("OnExecute name:", name)
 			for i := 0; i < arguments.Size(); i++ {
 				val := arguments.Get(i)
@@ -24,7 +24,7 @@ func Context(app cef.ICefApplication) {
 		fmt.Println("onHandler:", onHandler.Instance())
 		obj = cef.V8ValueRef.NewObject(nil, nil)
 		fmt.Println("obj:", obj.IsObject())
-		onFunc := cef.V8ValueRef.NewFunction("on", onHandler)
+		onFunc := cef.V8ValueRef.NewFunction("on", onHandler.AsInterface())
 		obj.SetValueByKey("on", onFunc, cef.V8_PROPERTY_ATTRIBUTE_READONLY)
 		context.GetGlobal().SetValueByKey("ipc", obj, cef.V8_PROPERTY_ATTRIBUTE_READONLY)
 	})
