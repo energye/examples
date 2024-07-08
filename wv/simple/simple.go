e1440e27c10c542b5a508f9c26afa817994bf437
yanghy <snxamdf@126.com>
2024-07-08 10:51:22 +0800

    v3: wv dev

--- wv/simple/simple.go
+++ wv/simple/simple.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"github.com/energye/assetserve"
 	"github.com/energye/energy/v3/ipc"
 	"github.com/energye/energy/v3/ipc/callback"
 	"github.com/energye/energy/v3/wv"
@@ -9,11 +11,15 @@ import (
 	"github.com/energye/lcl/types"
 )
 
+//go:embed resources
+var resources embed.FS
+
 func main() {
 	wv.Init(nil, nil)
 	app := wv.NewApplication()
 	app.SetOptions(wv.Options{
-		DefaultURL: "https://www.baidu.com",
+		//DefaultURL: "https://www.baidu.com",
+		DefaultURL: "http://localhost:22022",
 		//DisableContextMenu: true,
 		//DisableDevTools: true,
 	})
@@ -32,10 +38,19 @@ func main() {
 		})
 	})
 
-	ipc.On("test-name", func(event callback.IEvent) {
-
+	ipc.On("test-ipc", func(context callback.IContext) {
+		fmt.Println("context:", context.Data())
 	})
 	//ipc.RemoveOn("test-name")
-
+	startAssetsServer()
 	app.Run()
+}
+
+func startAssetsServer() {
+	server := assetserve.NewAssetsHttpServer()
+	server.PORT = 22022               //服务端口号
+	server.AssetsFSName = "resources" //必须设置目录名和资源文件夹同名
+	//server.Assets = resources
+	server.LocalAssets = "D:\\gopath\\src\\workspace\\examples\\wv\\simple\\resources"
+	go server.StartHttpServer()
 }
