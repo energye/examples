c31716f283822ee10190d059d35321328a06ca5a
杨红岩 <snxamdf@126.com>
2024-08-13 21:14:36 +0800

    v3: ipc, wv dev

--- wv/simple/simple.go
+++ wv/simple/simple.go
@@ -9,6 +9,7 @@ import (
 	"github.com/energye/energy/v3/wv"
 	_ "github.com/energye/examples/syso"
 	"github.com/energye/lcl/lcl"
+	"github.com/energye/lcl/rtl/version"
 	"github.com/energye/lcl/tools/exec"
 	"github.com/energye/lcl/types"
 	"math/rand"
@@ -21,19 +22,24 @@ var resources embed.FS
 
 func main() {
 	wv.Init(nil, nil)
+	fmt.Println("version:", version.OSVersion.ToString())
 	app := wv.NewApplication()
+	icon, _ := resources.ReadFile("resources/icon.ico")
 	app.SetOptions(wv.Options{
 		Caption: "energy - webview2",
 		//DefaultURL: "https://www.baidu.com",
 		//DefaultURL: "https://ap2.baoleitech.com:2443/bl-viewer-v1/dicompat?hcode=3122401&puid=01J4183BSWGJCPSB60ZX5EETM9&accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjMiLCJuYW1lIjoiYWRtaW4iLCJlbWFpbCI6ImFkbWluQGhvdG1haWwuY29tIiwiZGlzcGxheU5hbWUiOiLnrqHnkIblkZgiLCJpYXQiOjE3MjIzMjc0MjMsImV4cCI6MTcyMjM0OTAyM30.RpjkzTQDVb0l6qDtky7tI_4gNOyJoA2kTB7PudDAdzM",
 		DefaultURL: "http://localhost:22222/index.html",
+		Windows: wv.Windows{
+			ICON: icon,
+		},
 		//DisableContextMenu: true,
 		//DisableDevTools: true,
-		Frameless: true,
+		//Frameless: true,
 		//DisableResize:   true,
 		//DisableMinimize: true,
 		//DisableMaximize: true,
-		DefaultWindowStatus: types.WsFullScreen,
+		//DefaultWindowStatus: types.WsFullScreen,
 
 		//DisableWebkitAppRegionDClk: true,
 	})
