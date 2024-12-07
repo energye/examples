6b305f9e1cbb391d55a228746c47fb3d64ee0684
yanghy <snxamdf@126.com>
2024-12-07 18:23:24 +0800

    MacOS(Webkit2) Demo debug_most

--- wv/darwin/debug_most/debug_most.go
+++ wv/darwin/debug_most/debug_most.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"github.com/energye/assetserve"
 	_ "github.com/energye/examples/syso"
+	"github.com/energye/lcl/api"
 	"github.com/energye/lcl/lcl"
+	"github.com/energye/lcl/tools"
 	"github.com/energye/lcl/types"
 	"github.com/energye/wv/darwin"
 	"path/filepath"
@@ -20,11 +22,10 @@ type TMainForm struct {
 	webview       wv.IWkWebview
 	canClose      bool
 	isMainWindow  bool
+	contextMenu   lcl.IPopupMenu
 }
 
-var (
-	mainForm TMainForm
-)
+var mainForm TMainForm
 
 //go:embed assets
 var resources embed.FS
@@ -41,32 +42,95 @@ func main() {
 	lcl.Application.Run()
 }
 
-func (m *TMainForm) FormCreate(sender lcl.IObject) {
-	fmt.Println("main create")
-	icod, _ := resources.ReadFile("assets/icon.ico")
-	m.Icon().LoadFromBytes(icod)
-	m.SetCaption("Main")
-	m.SetWidth(800)
-	m.SetHeight(600)
-	m.SetDoubleBuffered(true)
-
+func (m *TMainForm) CreateContextMenu() {
 	// TPopupMenu
-	pm := lcl.NewPopupMenu(m)
+	m.contextMenu = lcl.NewPopupMenu(m)
 	item := lcl.NewMenuItem(m)
 	item.SetCaption("退出(&E)")
 	item.SetOnClick(func(lcl.IObject) {
 		m.Close()
 	})
-	pm.Items().Add(item)
+	m.contextMenu.Items().Add(item)
 	item = lcl.NewMenuItem(m)
 	item.SetCaption("Test")
 	item.SetOnClick(func(lcl.IObject) {
 		fmt.Println("test")
 	})
-	pm.Items().Add(item)
+	m.contextMenu.Items().Add(item)
 
 	// 将窗口设置一个弹出菜单，右键单击就可显示
-	m.SetPopupMenu(pm)
+	m.SetPopupMenu(m.contextMenu)
+}
+
+func (m *TMainForm) CreateMainMenu() {
+	mainMenu := lcl.NewMainMenu(m)
+	// 创建一级菜单
+	fileClassA := lcl.NewMenuItem(m)
+	fileClassA.SetCaption("文件(&F)") //菜单名称 alt + f
+	aboutClassA := lcl.NewMenuItem(m)
+	aboutClassA.SetCaption("关于(&A)")
+
+	var createMenuItem = func(label, shortCut string, click func(lcl.IObject)) (result lcl.IMenuItem) {
+		result = lcl.NewMenuItem(m)
+		result.SetCaption(label)                          //菜单项显示的文字
+		result.SetShortCut(api.DTextToShortCut(shortCut)) // 快捷键
+		result.SetOnClick(click)                          // 触发事件，回调函数
+		return
+	}
+	// 给一级菜单添加菜单项
+	createItem := createMenuItem("新建(&N)", "Meta+N", func(lcl.IObject) {
+		fmt.Println("单击了新建")
+	})
+	fileClassA.Add(createItem) // 把创建好的菜单项添加到 第一个菜单中
+	openItem := createMenuItem("打开(&O)", "Meta+O", func(lcl.IObject) {
+		fmt.Println("单击了打开")
+	})
+	fileClassA.Add(openItem)
+	mainMenu.Items().Add(fileClassA)
+	mainMenu.Items().Add(aboutClassA)
+	if tools.IsDarwin() {
+		// https://wiki.lazarus.freepascal.org/Mac_Preferences_and_About_Menu
+		// 动态添加的，静态好像是通过设计器将顶级的菜单标题设置为应用程序名，但动态的就是另一种方式
+		appMenu := lcl.NewMenuItem(m)
+		// 动态添加的，设置一个Unicode Apple logo char
+		appMenu.SetCaption(types.AppleLogoChar)
+		subItem := lcl.NewMenuItem(m)
+
+		subItem.SetCaption("关于")
+		subItem.SetOnClick(func(sender lcl.IObject) {
+			lcl.ShowMessage("About")
+		})
+		appMenu.Add(subItem)
+
+		subItem = lcl.NewMenuItem(m)
+		subItem.SetCaption("-")
+		appMenu.Add(subItem)
+
+		subItem = lcl.NewMenuItem(m)
+		subItem.SetCaption("首选项")
+		subItem.SetShortCut(api.DTextToShortCut("Meta+,"))
+		subItem.SetOnClick(func(sender lcl.IObject) {
+			lcl.ShowMessage("Preferences")
+		})
+		appMenu.Add(subItem)
+		// 添加
+		mainMenu.Items().Insert(0, appMenu)
+	}
+}
+
+func (m *TMainForm) FormCreate(sender lcl.IObject) {
+	fmt.Println("main create")
+	icon, _ := resources.ReadFile("assets/icon.ico")
+	m.Icon().LoadFromBytes(icon)
+	m.SetCaption("Main")
+	m.SetWidth(800)
+	m.SetHeight(600)
+	m.SetDoubleBuffered(true)
+
+	if m.isMainWindow {
+		m.CreateMainMenu()
+	}
+	m.CreateContextMenu()
 
 	m.webview = wv.NewWkWebview(m)
 	m.webview.SetOnProcessMessage(func(sender wv.IObject, userContentController wv.WKUserContentController, name, message string) {
@@ -76,7 +140,7 @@ func (m *TMainForm) FormCreate(sender lcl.IObject) {
 		}{}
 		json.Unmarshal([]byte(message), &messageData)
 		if messageData.Name == "contextmenu" {
-			pm.PopUp()
+			m.contextMenu.PopUp()
 		}
 	})
 	m.webview.SetOnStartProvisionalNavigation(func(sender wv.IObject, navigation wv.WKNavigation) {
@@ -112,7 +176,9 @@ func (m *TMainForm) FormCreate(sender lcl.IObject) {
 		//	url.Free()
 		//}
 	})
+	// 打开一个新窗口时触发事件
 	m.webview.SetOnCreateWebView(m.OnCreateWebView)
+
 	m.webview.SetOnStartURLSchemeTask(m.OnStartURLSchemeTask)
 	m.webview.SetOnStopURLSchemeTask(m.OnStopURLSchemeTask)
 
@@ -124,6 +190,7 @@ func (m *TMainForm) FormCreate(sender lcl.IObject) {
 
 	userContentController := wv.WKUserContentControllerRef.New()
 	scriptMessageHandler := wv.NewWKScriptMessageHandler(m.webview.AsReceiveScriptMessageDelegate())
+	// 自定义ipc进程消息对象(在js使用)
 	userContentController.AddScriptMessageHandlerName(scriptMessageHandler, "processMessage")
 
 	configuration := wv.WKWebViewConfigurationRef.New()
@@ -133,6 +200,7 @@ func (m *TMainForm) FormCreate(sender lcl.IObject) {
 
 	configuration.SetSuppressesIncrementalRendering(true)
 	configuration.SetApplicationNameForUserAgent("energy.cn")
+	// 自定义 url 协议
 	configuration.SetURLSchemeHandlerForURLScheme(URLSchemeHandler.Data(), "energy")
 
 	preference := wv.NewWKPreferences(configuration.Preferences()) //wv.WKPreferencesRef.New()
@@ -143,7 +211,7 @@ func (m *TMainForm) FormCreate(sender lcl.IObject) {
 	preference.EnableDevtools()
 
 	navigationDelegate := wv.NewWKNavigationDelegate(m.webview.AsWKNavigationDelegate())
-	UIDelegate := wv.NewWKUIDelegate(m.webview.AsWKUIDelegate())
+	uiDelegate := wv.NewWKUIDelegate(m.webview.AsWKUIDelegate())
 
 	frame := &wv.TRect{}
 	frame.SetWidth(m.Width())
@@ -151,21 +219,17 @@ func (m *TMainForm) FormCreate(sender lcl.IObject) {
 	m.webview.InitWithFrameConfiguration(frame, configuration.Data())
 
 	m.webview.SetNavigationDelegate(navigationDelegate.Data())
-	m.webview.SetUIDelegate(UIDelegate.Data())
+	m.webview.SetUIDelegate(uiDelegate.Data())
 
 	// end
 	m.webviewParent.SetWebview(m.webview.Data())
 
 	m.SetOnShow(func(sender lcl.IObject) {
 		fmt.Println("OnShow:", m.url)
-		//m.webview.LoadURL("https://energye.github.io")
-		//m.webview.LoadURL("http://localhost:22022/index.html")
-		//m.webview.LoadURL(m.url)
 		if m.url != "" {
 			m.webview.LoadURL(m.url)
 		}
 		m.ScreenCenter()
-		//		preference.SetValueForKey(true, "developerExtrasEnabled")
 	})
 	m.webview.SetOnWebContentProcessDidTerminate(func(sender wv.IObject) {
 		fmt.Println("OnWebContentProcessDidTerminate")
