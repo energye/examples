216948407503c92a13fc8da814f1ec48d24c0e5d
yanghy <snxamdf@126.com>
2025-02-13 17:05:05 +0800

    修改wv示例的开发环境加载库

--- wv/windows/wv2load/wv2load.go
+++ wv/windows/wv2load/wv2load.go
@@ -2,11 +2,10 @@ package wv2load
 
 import (
 	"github.com/energye/lcl/api/libname"
-	"path"
+	"path/filepath"
 )
 
 func Wv2Load() (string, string) {
-	wv2Path := libname.GetLibPath("WebView2Loader.dll")
-	home, _ := path.Split(wv2Path)
-	return home, wv2Path
+	wv2Path, _ := filepath.Split(libname.LibName)
+	return wv2Path, filepath.Join(wv2Path, "WebView2Loader.dll")
 }
