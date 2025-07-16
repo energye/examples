219b00f98862c839efa65333b09a00d1e1b9e673
yanghy <snxamdf@126.com>
2025-07-16 18:02:24 +0800

    callback

--- lcl/action/main.go
+++ lcl/action/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"github.com/energye/examples/lcl/action/src"
 	. "github.com/energye/examples/syso"
-	"github.com/energye/lcl/inits"
+	"github.com/energye/lcl/api/libname"
 	"github.com/energye/lcl/lcl"
 )
 
 func main() {
-	lcl.DEBUG = true
-	inits.Init(nil, nil)
+	libname.LibName = ""
+	lcl.Init(nil, nil)
 	lcl.Application.Initialize()
 	lcl.Application.SetMainFormOnTaskBar(true)
 	lcl.Application.CreateForm(&src.MainForm)
