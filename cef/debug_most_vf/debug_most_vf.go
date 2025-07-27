ac44d222477532a237454d3ac372eb9574039609
yanghy <snxamdf@126.com>
2025-07-27 22:47:24 +0800

    Static CEF API Test

--- cef/debug_most_vf/debug_most_vf.go
+++ cef/debug_most_vf/debug_most_vf.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/energye/cef/cef"
+	cefTypes "github.com/energye/cef/types"
 	"github.com/energye/examples/cef/debug_most/application"
-	_ "github.com/energye/examples/syso"
+	. "github.com/energye/examples/syso"
 	"github.com/energye/lcl/lcl"
-	"github.com/energye/lcl/process"
-	"github.com/energye/lcl/tools"
+	"github.com/energye/lcl/tool"
 )
+
+func init() {
+	TestLoadLibPath()
+}
 
 func main() {
 	//全局初始化 每个应用都必须调用的
@@ -17,22 +21,22 @@ func main() {
 	// CEF message loop
 	app.SetExternalMessagePump(false)
 	app.SetMultiThreadedMessageLoop(false)
-	if tools.IsDarwin() {
+	if tool.IsDarwin() {
 		app.SetUseMockKeyChain(true)
 		app.InitLibLocationFromArgs()
 		// MacOS使用扩展消息泵
 		cef.AddCrDelegate()
-		cef.GlobalWorkSchedulerCreate(nil)
+		scheduler := cef.NewWorkScheduler(nil)
+		cef.SetGlobalCEFWorkSchedule(scheduler)
 		app.SetOnScheduleMessagePumpWork(nil)
-		if !process.Args.IsMain() {
+		if app.ProcessType() != cefTypes.PtBrowser {
 			// MacOS 多进程时，需要调用StartSubProcess来启动子进程
 			subStart := app.StartSubProcess()
-			fmt.Println("subStart:", subStart, process.Args.ProcessType())
-			app.Free()
+			fmt.Println("subStart:", subStart, app.ProcessType())
 			return
 		}
 	}
-	if tools.IsLinux() {
+	if tool.IsLinux() {
 		// 这是一个解决“GPU不可用错误”问题的方法 linux
 		// https://bitbucket.org/chromiumembedded/cef/issues/2964/gpu-is-not-usable-error-during-cef
 		app.SetDisableZygote(true)
@@ -41,11 +45,11 @@ func main() {
 		fmt.Println("SetOnContextInitialized")
 		component := lcl.NewComponent(nil)
 		chromium := cef.NewChromium(component)
-		windowComponent := cef.NewCEFWindowComponent(component)
-		viewComponent := cef.NewCEFBrowserViewComponent(component)
+		windowComponent := cef.NewWindowComponent(component)
+		viewComponent := cef.NewBrowserViewComponent(component)
 		url := "https://gitee.com/energye/energy"
-		windowComponent.SetOnWindowCreated(func(sender cef.IObject, window cef.ICefWindow) {
-			ok := chromium.CreateBrowserByBrowserViewComponent(url, viewComponent, nil, nil)
+		windowComponent.SetOnWindowCreated(func(sender lcl.IObject, window cef.ICefWindow) {
+			ok := chromium.CreateBrowserWithStringBrowserViewComponentRequestContextDictionaryValue(url, viewComponent, nil, nil)
 			fmt.Println("SetOnWindowCreated CreateBrowserByBrowserViewComponent:", true)
 			if ok {
 				windowComponent.AddChildView(viewComponent.BrowserView())
@@ -53,18 +57,17 @@ func main() {
 				windowComponent.Show()
 			}
 		})
-		chromium.SetOnBeforeClose(func(sender cef.IObject, browser cef.ICefBrowser) {
+		chromium.SetOnBeforeClose(func(sender lcl.IObject, browser cef.ICefBrowser) {
 			app.QuitMessageLoop()
 		})
 		windowComponent.CreateTopLevelWindow()
 	})
 	mainStart := app.StartMainProcess()
-	fmt.Println("mainStart:", mainStart, process.Args.ProcessType())
+	fmt.Println("mainStart:", mainStart, app.ProcessType())
 	if mainStart {
 		// 结束应用后释放资源
 		app.RunMessageLoop()
 		fmt.Println("app free")
-		app.Free()
 	}
 	///usr/share/lazarus/3.2.0/lcl/graphics.pp
 }
