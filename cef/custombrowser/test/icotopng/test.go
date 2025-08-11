c16d5348ba6e78289b5c6074bf642459d598a921
yanghy <snxamdf@126.com>
2025-08-11 20:20:46 +0800

    lcl widget

---
+++ cef/custombrowser/test/icotopng/test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/energye/examples/cef/utils"
+	"image/png"
+	"os"
+)
+
+func main() {
+	// 1. 打开ICO文件
+	icoFile, err := os.Open("E:\\SWT\\gopath\\src\\github.com\\energye\\workspace\\examples\\EnergyCache\\SiteResource\\www.bilibili.com_favicon.ico")
+	if err != nil {
+		panic(err)
+	}
+	defer icoFile.Close()
+
+	// 2. 解码ICO（自动选择最佳尺寸）
+	img, err := utils.Decode(icoFile)
+	if err != nil {
+		panic(err)
+	}
+
+	// 3. 创建PNG输出文件
+	pngFile, err := os.Create("E:\\SWT\\gopath\\src\\github.com\\energye\\workspace\\examples\\cef\\custombrowser\\test\\xxx.png")
+	if err != nil {
+		panic(err)
+	}
+	defer pngFile.Close()
+
+	// 4. 编码为PNG格式
+	if err := png.Encode(pngFile, img); err != nil {
+		panic(err)
+	}
+}
---
+++ cef/custombrowser/test/pngscale/pngscale.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/energye/examples/cef/utils/draw"
+	"image"
+	"image/png"
+	"os"
+)
+
+func main() {
+	// 1. 读取原始图片
+	file, err := os.Open("E:\\SWT\\gopath\\src\\github.com\\energye\\workspace\\examples\\cef\\custombrowser\\test\\xxx.png")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	img, err := png.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+
+	// 2. 计算目标尺寸（例如缩小到 50%）
+	originalBounds := img.Bounds()
+	newWidth := 16
+	newHeight := 16
+
+	// 3. 创建目标图像（RGBA 格式）
+	scaledImg := image.NewRGBA(image.Rect(0, 0, newWidth, newHeight))
+
+	// 4. 使用 CatmullRom 插值（比双线性更平滑）
+	draw.CatmullRom.Scale(
+		scaledImg,
+		scaledImg.Bounds(),
+		img,
+		originalBounds,
+		draw.Over,
+		nil,
+	)
+
+	// 5. 保存结果
+	outFile, err := os.Create("E:\\SWT\\gopath\\src\\github.com\\energye\\workspace\\examples\\cef\\custombrowser\\test\\output.png")
+	if err != nil {
+		panic(err)
+	}
+	defer outFile.Close()
+
+	png.Encode(outFile, scaledImg)
+}
