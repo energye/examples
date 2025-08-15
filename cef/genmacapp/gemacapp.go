596d50e83f962b37c2056e9bcf42724082e623fe
yanghy <snxamdf@126.com>
2025-08-15 08:27:29 +0800

    lcl widget macos

--- cef/genmacapp/gemacapp.go
+++ cef/genmacapp/gemacapp.go
@@ -3,7 +3,7 @@ package main
 import "github.com/energye/lcl/pkgs/mac"
 
 func main() {
-	mainExe := "/Users/yanghy/app/workspace/examples/cef/debug_most/go_build_cef_debug_most_go"
+	mainExe := "/Users/yanghy/app/workspace/examples/cef/debug_most/debug_most"
 	subExe := "/Users/yanghy/app/workspace/examples/cef/debug_most/helper/helper"
 	mac.GenApp(mainExe, subExe)
 }
