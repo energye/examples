167637e81636d7ba7d18bcd27f244383079f0d2e
yanghy <snxamdf@126.com>
2026-01-03 22:38:31 +0800

    energy webview

--- wv/windows/simple/simple.go
+++ wv/windows/simple/simple.go
@@ -3,13 +3,14 @@ package main
 import (
 	"github.com/energye/energy/v3/application"
 	"github.com/energye/energy/v3/wv"
-	_ "github.com/energye/examples/syso"
 	"github.com/energye/examples/wv/windows/simple/app"
+	. "github.com/energye/examples/wv/windows/simple/resources"
 )
 
 func main() {
 	wvApp := wv.Init(nil, nil)
 	wvApp.SetOptions(application.Options{DefaultURL: "https://www.baidu.com"})
-	//wvApp.SetOptions(application.Options{})
+	//wvApp.SetOptions(application.Options{DefaultURL: "about:blank"})
+	SetIcon()
 	wv.Run(app.Forms...)
 }
