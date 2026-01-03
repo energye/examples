c1adc20d2227995847aba81b7785d65763bb7fb4
yanghy <snxamdf@126.com>
2026-01-03 21:03:32 +0800

    energy webview

--- wv/windows/simple/simple.go
+++ wv/windows/simple/simple.go
@@ -5,22 +5,11 @@ import (
 	"github.com/energye/energy/v3/wv"
 	_ "github.com/energye/examples/syso"
 	"github.com/energye/examples/wv/windows/simple/app"
-	"github.com/energye/lcl/lcl"
 )
 
 func main() {
 	wvApp := wv.Init(nil, nil)
 	wvApp.SetOptions(application.Options{DefaultURL: "https://www.baidu.com"})
-	wvApp.Start()
 
-	// 初始化应用程序实例
-	lcl.Application.Initialize()
-	// 配置应用程序设置，使主窗体在Windows任务栏上显示
-	lcl.Application.SetMainFormOnTaskBar(true)
-	// 启用自动缩放功能以支持高DPI显示器
-	lcl.Application.SetScaled(true)
-	// 创建所有窗体
-	lcl.Application.NewForms(&app.Form1Window)
-	// 启动应用程序消息循环
-	lcl.Application.Run()
+	wv.Run(app.Forms...)
 }
