aeee6c44fd831135cdf2dc74668ef7db80bb9ec4
yanghy <snxamdf@126.com>
2026-01-06 18:55:21 +0800

    energy webkit2

---
+++ wv/linux/simple/simple.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/energye/energy/v3/application"
+	"github.com/energye/energy/v3/wv"
+	"github.com/energye/examples/wv/windows/simple/app"
+	. "github.com/energye/examples/wv/windows/simple/resources"
+)
+
+func main() {
+	wvApp := wv.Init(nil, nil)
+	wvApp.SetOptions(application.Options{DefaultURL: "https://www.baidu.com", Caption: "Test Energy"})
+	SetIcon()
+	wv.Run(app.Forms...)
+}
