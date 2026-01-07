038d3808bfeca18ea5ca7bd6d0246bc9f5386c86
yanghy <snxamdf@126.com>
2026-01-07 20:40:24 +0800

    energy webkit2

--- wv/windows/local_load/resources/test.js
+++ wv/windows/local_load/resources/test.js
@@ -2,3 +2,12 @@ function myAlertFunction() {
     alert("Hello! I am an alert box!!");
 }
 
+(function () {
+    console.log('test.js')
+    console.log(energy)
+    console.log(ipc)
+})()
+
+window.setTimeout(function () {
+
+}, 1000)
