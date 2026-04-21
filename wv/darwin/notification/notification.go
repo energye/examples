3ccaed8484d54296392ed1cc573649700b4a0153
yanghy <snxamdf@126.com>
2026-04-21 18:20:47 +0800

    macos Notification

--- wv/darwin/notification/notification.go
+++ wv/darwin/notification/notification.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/energye/examples/wv/darwin/notification/src"
+	"github.com/energye/lcl/api"
 	"github.com/energye/lcl/lcl"
 )
 
 // codesign --force --deep --sign - notification.app
 // codesign -dv notification.app
 func main() {
+	api.SetDebug(true)
 	lcl.Init(nil, nil)
 	lcl.Application.Initialize()
 	lcl.Application.SetMainFormOnTaskBar(true)
