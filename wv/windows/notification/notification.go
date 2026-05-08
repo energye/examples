af5bd86ded6c841f2966bd19fc4a82e051f6df30
yanghy <snxamdf@126.com>
2026-05-08 08:54:43 +0800

    MTA thread for COM windows notification

--- wv/windows/notification/notification.go
+++ wv/windows/notification/notification.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "github.com/energye/examples/syso"
+	"github.com/energye/examples/wv/windows/notification/resources"
 	"github.com/energye/examples/wv/windows/notification/src"
 	"github.com/energye/lcl/api"
 	"github.com/energye/lcl/lcl"
@@ -12,6 +13,7 @@ func main() {
 	lcl.Init(nil, nil)
 	lcl.Application.Initialize()
 	lcl.Application.SetMainFormOnTaskBar(true)
+	resources.SetIcon()
 	lcl.Application.NewForm(&src.MainForm)
 	lcl.Application.Run()
 }
