1bbf7b22f131dee120c16d7f56240747743f5a26
yanghy <snxamdf@126.com>
2026-05-09 22:32:39 +0800

    simple_switch_parent

---
+++ wv/linux/simple_switch_parent/app/form1.go
@@ -0,0 +1,38 @@
+// ==============================================================================
+// 📚 form1.go 用户代码文件
+// 📌 该文件不存在时自动创建
+// ✏️ 可在此文件中添加事件处理和业务逻辑
+//    生成时间: 2026-04-03 15:44:27
+// ==============================================================================
+
+package app
+
+import (
+	"github.com/energye/lcl/lcl"
+	"github.com/energye/lcl/types"
+)
+
+// OnFormCreate 窗体初始化事件
+func (m *TForm1) OnFormCreate(sender lcl.IObject) {
+	// TODO 在此处添加窗体初始化代码
+	m.Webview1.SetWindow(m)
+	//m.Webview1.SetDefaultURL("https://energye.gitee.io")
+}
+
+// OnFormShow 窗体显示事件
+func (m *TForm1) OnFormShow(sender lcl.IObject) {
+	// TODO 在此处添加窗体显示事件代码
+	m.Webview1.CreateBrowser()
+}
+
+// OnFormCloseQuery 窗体关闭前询问事件
+func (m *TForm1) OnFormCloseQuery(sender lcl.IObject, canClose *bool) bool {
+	// TODO 在此处添加窗体关闭前询问代码
+	return false
+}
+
+// OnFormClose 仅当 OnCloseQuery 中 CanClose 被设置为 True 后会触发
+func (m *TForm1) OnFormClose(sender lcl.IObject, closeAction *types.TCloseAction) bool {
+	// TODO 在此处添加窗体关闭代码
+	return false
+}
---
+++ wv/linux/simple_switch_parent/simple_switch_parent.go
@@ -0,0 +1,29 @@
+// ==============================================================================
+// 📚 应用启动入口文件
+// 📌 该文件在创建项目时创建
+// ✏️ 可在此文件中添加业务逻辑
+// ==============================================================================
+
+package main
+
+import (
+	"github.com/energye/energy/v3/application"
+	"github.com/energye/energy/v3/wv"
+	"github.com/energye/examples/wv/linux/simple_switch_parent/app"
+	"github.com/energye/lcl/api"
+	"github.com/energye/lcl/api/libname"
+)
+
+func main() {
+	api.SetDebug(true)
+	libname.LibName = "/home/yanghy/app/workspace/gen/gout/libenergy-gtk3.so"
+	// 全局初始化
+	wvApp := wv.Init(nil, nil)
+	wvApp.SetOptions(application.Options{
+		DefaultURL: "https://energye.gitee.io/",
+		Linux:      application.Linux{HardwareGPU: application.HGPUDisable}, // VM WARE
+		//Linux: application.Linux{HardwareGPU: application.HGPUEnable}, // GPU Device
+	})
+	// 启动应用程序消息循环
+	wv.Run(app.Forms...)
+}
