5d5596f7ee05cd41380a346f31ed714ba5fdf856
yanghy <snxamdf@126.com>
2026-05-29 19:40:37 +0800

    国际化优化，更方便的使用

--- cef/debug_most/helper/helper.go
+++ cef/debug_most/helper/helper.go
@@ -1,19 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"github.com/energye/cef/cef"
-	"github.com/energye/examples/cef/debug_most/helper/internal"
-	"github.com/energye/lcl/api/exception"
-)
-
 func main() {
-	cef.Init()
-	exception.SetOnException(func(exception int32, message string) {
-		fmt.Println("[ERROR] exception:", exception, "message:", message)
-	})
-	//创建Cef应用
-	app := internal.InitApplication()
-	ok := app.StartSubProcess()
-	println("sub process start:", ok)
+	//cef.Init()
+	//exception.SetOnException(func(exception int32, message string) {
+	//	fmt.Println("[ERROR] exception:", exception, "message:", message)
+	//})
+	////创建Cef应用
+	//app := internal.InitApplication()
+	//ok := app.StartSubProcess()
+	//println("sub process start:", ok)
 }
