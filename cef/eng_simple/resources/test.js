ef76f1acc3326ac0339911a379ed7e85acbe2eb2
yanghy <snxamdf@126.com>
2026-06-18 09:48:21 +0800

    v3.0: Add CEF for energy

---
+++ cef/eng_simple/resources/test.js
@@ -0,0 +1,23 @@
+function myAlertFunction() {
+    alert("Hello! I am an alert box!!");
+}
+
+(function () {
+    console.log('test.js')
+    console.log(energy)
+    console.log(ipc)
+    console.log(window)
+})()
+
+window.setTimeout(function () {
+    if (ipc) {
+        console.log('ipc.emit-test')
+        ipc.emit("test", [new Date().toString()], function (res1, res2, res3, res4, res5) {
+            console.log("result:", res1, res2, res3, res4, res5, "now:", new Date().toString())
+        })
+    }
+}, 1000)
+
+ipc.on('test', function (data) {
+    console.log('ipc.on-test:',data)
+})
